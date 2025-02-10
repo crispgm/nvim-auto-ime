c3459f7f985cce0ed4da896d34e22c808a5bba85
David Zhang <crispgm@gmail.com>
2025-02-10 11:58:35 +0800

    feat: check macism before perform any actions

--- lua/auto-ime/selector.lua
+++ lua/auto-ime/selector.lua
@@ -2,18 +2,41 @@ local M = {}
 
 local config = require('auto-ime.config')
 
+local check_dep = function()
+    if vim.fn.executable('macism') ~= 1 then
+        vim.api.nvim_echo({
+            { 'nvim-auto-ime', 'ErrorMsg' },
+            { ' ' .. 'macism not installed' },
+        }, true, {})
+
+        return false
+    end
+
+    return true
+end
+
 local switch_ime = function(ime)
+    if check_dep() == false then
+        return false
+    end
     vim.fn.system(string.format('macism %s', ime))
+    return true
 end
 
 local current_ime = function()
+    if check_dep() == false then
+        return false
+    end
     local current = vim.fn.system('macism')
     vim.g.auto_ime_current = current
+
+    return true
 end
 
 M.current_ime = function()
-    current_ime()
-    print(vim.g.auto_ime_current)
+    if current_ime() == true then
+        print(vim.g.auto_ime_current)
+    end
 end
 
 M.toggle_ime = function()
@@ -25,13 +48,17 @@ M.toggle_ime = function()
 end
 
 M.switch_ime_default = function()
-    switch_ime(config.options.ime_default)
+    if switch_ime(config.options.ime_default) == false then
+        return
+    end
     vim.g.auto_ime_selected = 'default'
     current_ime()
 end
 
 M.switch_ime_alt = function()
-    switch_ime(config.options.ime_source)
+    if switch_ime(config.options.ime_source) == false then
+        return
+    end
     vim.g.auto_ime_selected = 'alt'
     current_ime()
 end
